f72e4bf19e460644123d751fc024c76aa625391d
MikeyGee Desktop <mikeygee132@gmail.com>
2022-08-05 15:03:12 -0500

    Player mechanics started.

--- fg-script.js
+++ fg-script.js
@@ -1,3 +1,4 @@
+// ------- canvas properties ------------------------------------------------------------- A
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -6,26 +7,32 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+// ------- Sprite properties -------------------------------------------------------------
+
+const gravity = 0.2;
+
 class Sprite {
-  constructor({position, velocity}) {
-    this.position = position
-    this.velocity = velocity
-    this.height = 150
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
   }
 
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, this.height)
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
   }
 
   update() {
-    this.draw()
-    this.position.y += this.velocity.y
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-        this.velocity.y = 0
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
-    
   }
 }
 
@@ -36,29 +43,100 @@ const player = new Sprite({
   },
   velocity: {
     x: 0,
-    y: 10,
+    y: 0,
   },
 });
 
 const enemy = new Sprite({
   position: {
-    x: 400,
+    x: 974,
     y: 100,
   },
   velocity: {
     x: 0,
-    y: 15,
+    y: 0,
   },
 });
 
+// ------- Animation properties -------------------------------------------------------------
+
+const keys = {
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+};
+
+let lastKey;
 
 function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    enemy.update()
-  }
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
 
-  animate()
-  
+  player.velocity.x = 0;
+
+  if (keys.ArrowLeft.pressed && lastKey === "ArrowLeft") {
+    player.velocity.x = -1;
+  } else if (keys.ArrowRight.pressed && lastKey === "ArrowRight") {
+    player.velocity.x = 1;
+  }
+}
+
+animate();
+
+// ------- KEY DOWN properties -------------------------------------------------------------
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      lastKey = "ArrowLeft";
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;                 //  ____
+      lastKey = "ArrowRight";                         // /_   |
+      break;                                          //  |   |
+    case "ArrowUp":                                   //  |   |
+      player.velocity.y = -10;                       //   |   |
+      break;                                         //   |___|
+
+    // enemy movement -----------------------------------------------------------------------
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+  console.log(event.key);
+});
+
+// ------- KEY UP properties -------------------------------------------------------------
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowUp":
+      keys.ArrowUp.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
