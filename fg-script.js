ce3554fe96393add3789aabc438a79d2670b4ebc
MikeyGee Desktop <mikeygee132@gmail.com>
2022-09-13 15:40:24 -0500

    added health bars and timer

--- fg-script.js
+++ fg-script.js
@@ -39,7 +39,7 @@ class Sprite {
 
     // --------------------------------------ATTACK BOX --------------------------------
     
-    //if (this.isAttacking) {
+    if (this.isAttacking) {
     c.fillStyle = "green";
     c.fillRect(
       this.attackBox.position.x, 
@@ -47,7 +47,7 @@ class Sprite {
       this.attackBox.width, 
       this.attackBox.height
       )
-    //}
+    }
   }
 
 
